708011f719894c4454f9d782c531911396eaa3bd
samorvell <samuel_amaro_4@hotmail.com>
2022-11-12 19:55:07 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/samorvell/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/samorvell/hrpayroll/resources/PaymentResource.java
@@ -1,6 +1,7 @@
 package com.samorvell.hrpayroll.resources;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.samorvell.hrpayroll.entities.Payment;
 import com.samorvell.hrpayroll.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = paymentService.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+        Payment payment = new Payment("Brocolis",400.0, days);
         return ResponseEntity.ok(payment);
     }
 
