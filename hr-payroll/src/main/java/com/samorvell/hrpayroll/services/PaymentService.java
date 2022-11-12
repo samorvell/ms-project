5cf4589c9d5b3a2cb316ef96c0ba9611dc5ee08b
samorvell <samuel_amaro_4@hotmail.com>
2022-11-12 14:54:01 -0300

    Ribbon load balacing

--- hr-payroll/src/main/java/com/samorvell/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/samorvell/hrpayroll/services/PaymentService.java
@@ -4,12 +4,7 @@ import com.samorvell.hrpayroll.entities.Payment;
 import com.samorvell.hrpayroll.entities.Worker;
 import com.samorvell.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
--- hr-worker/src/main/java/com/samorvell/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/samorvell/hrworker/resources/WorkerResource.java
@@ -2,7 +2,11 @@ package com.samorvell.hrworker.resources;
 
 import com.samorvell.hrworker.entities.Worker;
 import com.samorvell.hrworker.repositories.WorkerRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +19,11 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger logger= LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository workerRepository;
 
@@ -26,6 +35,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Worker worker = workerRepository.findById(id).get();
         return ResponseEntity.ok(worker);
     }
