ede15604676051d56aab2f46a1effee5ab7e2b78
samorvell <samuel_amaro_4@hotmail.com>
2022-11-12 14:03:51 -0300

    Feign client

--- hr-payroll/src/main/java/com/samorvell/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/samorvell/hrpayroll/HrPayrollApplication.java
@@ -2,7 +2,10 @@ package com.samorvell.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
