2232e5c4348fff24983e208983f6835588798502
samorvell <samuel_amaro_4@hotmail.com>
2022-11-15 12:53:47 -0300

    User, Role, many to many association

---
+++ hr-user/src/main/java/com/samorvell/hruser/entities/Role.java
@@ -0,0 +1,19 @@
+package com.samorvell.hruser.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "role")
+public class Role implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+
+}
---
+++ hr-user/src/main/java/com/samorvell/hruser/entities/User.java
@@ -0,0 +1,32 @@
+package com.samorvell.hruser.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+            @JoinTable(name = "user_role",
+                    joinColumns = @JoinColumn(name = "user_id"),
+                    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+}
