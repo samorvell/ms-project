4fdfdfaa4e5be05a56476f8b8dceeb990200e7a8
samorvell <samuel_amaro_4@hotmail.com>
2022-11-15 21:14:45 -0300

    Configuring rh-oauth poject

--- hr-oauth/src/main/java/com/samovell/hroauth/AuthorizationServerApplication.java
+++ hr-oauth/src/main/java/com/samovell/hroauth/AuthorizationServerApplication.java
@@ -2,7 +2,9 @@ package com.samovell.hroauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AuthorizationServerApplication {
 
