5f181e6fe76a8a620e886b0d6e7458ef58cfe8d6
samorvell <samuel_amaro_4@hotmail.com>
2022-11-16 14:27:49 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/samovell/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/samovell/hroauth/entities/User.java
@@ -1,13 +1,18 @@
 package com.samovell.hroauth.entities;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
 
     public static final long serialVersionUID = 1L;
 
@@ -20,5 +25,36 @@ public class User implements Serializable {
 
     public Set<Role> getRoles() {
         return roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
